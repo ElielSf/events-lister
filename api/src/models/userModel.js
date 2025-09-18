2c0d59cb501962eaab75b92a0e4784cbae45c890
Eliel Souza <elielsf13@gmail.com>
2025-09-18 11:40:02 -0300

    fix: alter login query to return role

--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -2,6 +2,12 @@ import { connection } from "../config/db";
 
 //modelo para buscar o usuario no banco de dados
 export async function findByCPF(cpf) {
-  const [rows] = await connection.query("SELECT * FROM users WHERE cpf = ?", [cpf]);
+  const [rows] = await connection.query(
+    `SELECT u.id_user, u.user_name, u.user_password, r.role_name AS role
+  FROM users u
+  JOIN roles r ON u.roles_id_role = r.id_role
+  WHERE u.user_cpf = ?;`,
+    [cpf]
+  );
   return rows[0];
 }
