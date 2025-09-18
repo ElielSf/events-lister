6c7c6698b0ae60cf317e55e8be1850d04fe12167
Eliel Souza <elielsf13@gmail.com>
2025-09-18 10:46:12 -0300

    feat: create database connection

---
+++ api/src/config/db.js
@@ -0,0 +1,14 @@
+import { createPool } from "mysql2/promise";
+
+//importando variaveis de ambiente
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } from "./config";
+
+//criando conexao com o banco
+export const connection = createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+});
+
+
