d6796c9ff5b9d4cadc3e4a3d06349ad0c64d1e57
Eliel Souza <elielsf13@gmail.com>
2025-09-17 22:39:11 -0300

    add: start the project

---
+++ api/src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = 3000; 
+
+//criando o servidor
+const app = express();
+
+//middlewares de configuração do servidor
+app.use(express.json());
+app.use(cors());
+
+//rota de teste
+app.get('/', (req, res) => {
+    res.send('Servidor rodando!');
+});
+
+//iniciando o servidor
+const server = app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+//tratando erros
+server.on('error', (err) => {
+    console.error('Ocorreu um erro no servidor:', err);
+});
+
+
+
