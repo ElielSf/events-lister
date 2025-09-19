b46e1350f5e4d8bd8494172ce6b7f136c2675293
Eliel Souza <elielsf13@gmail.com>
2025-09-19 19:49:40 -0300

    feat: add register page

---
+++ web/src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,14 @@
+//importando componentes
+import Register from "../../components/Register/Register";
+
+//css
+import "./css/RegisterPage.css";
+
+//exportando pagina de Register
+export default function RegisterPage() {
+  return (
+    <div className="register-page">
+      <Register />
+    </div>
+  );
+}
