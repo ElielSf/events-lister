c190db26b43c0d9ad802fb35007d7527235ee277
Eliel Souza <elielsf13@gmail.com>
2025-09-19 14:32:28 -0300

    feat: add Login page

---
+++ web/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+//importando componentes
+import Login from "../../components/Login/Login";
+
+//css
+import "./css/LoginPage.css";
+
+//exportando pagina de login
+export default function LoginPage() {
+  return (
+    <div className="login-page">
+      <Login />
+    </div>
+  );
+}
