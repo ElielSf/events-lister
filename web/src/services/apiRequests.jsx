f27e242290fd64c72cfa7e39d233da1552d3ec6c
Eliel Souza <elielsf13@gmail.com>
2025-09-20 14:48:47 -0300

    feat: add a file with the get api requests

---
+++ web/src/services/apiRequests.jsx
@@ -0,0 +1,50 @@
+//url base da api
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+//funcao para fazer requisicao as rotas de busca de dados
+export const fetchData = async (endpoint, options = {}) => {
+  const token = localStorage.getItem("authToken");
+
+  //verifica se o usuario esta logado
+  if (!token) {
+    //redireciona para a pagina de login
+    window.location.href = "/login";
+
+    return null;
+  }
+
+  //configura o cabecalho da requisicao
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: token ? `Bearer ${token}` : undefined,
+    ...options.headers,
+  };
+
+  //faz a requisicao
+  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+    ...options,
+    headers,
+  });
+
+  //em caso do token ser invalido
+  if (response.status === 401 || response.status === 403) {
+    //limpa o token invalido
+    localStorage.removeItem("authToken");
+
+    //redireciona para a pagina de login
+    window.location.href = "/login";
+
+    return null;
+  }
+
+  //em caso de falha na requisicao
+  if (!response.ok) {
+    return null;
+  }
+
+  //em caso de sucesso na requisicao
+  return response.json();
+};
+
+//rotas especificas GET
+export const getAllPaymentMethods = () => fetchData("/payments/");
