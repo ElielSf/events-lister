f7c293dd72e6c70e08f67a2667cc03acc09cee80
Eliel Souza <elielsf13@gmail.com>
2025-09-20 13:32:26 -0300

    feat: add create function in payment controller

--- api/src/controllers/paymentController.js
+++ api/src/controllers/paymentController.js
@@ -1,4 +1,5 @@
 //importando os modelos
+import e from "cors";
 import {
   getPayments,
   createPayment,
@@ -7,7 +8,7 @@ import {
 } from "../models/paymentModel.js";
 
 //funcao para retornar os metodos de pagamento ativos no sistema
-export async function getAllPayments(req, res) {
+export async function getAllPaymentMethods(req, res) {
   try {
     //buscando os metodos de pagamento
     const payments = await getPayments();
@@ -16,11 +17,42 @@ export async function getAllPayments(req, res) {
     if (payments.length === 0) {
       return res
         .status(404)
-        .json({ error: "Nenhum método de pagamento encontrado." });
+        .json({ error: "Nenhum método de pagamento encontrado" });
     }
 
     //retorna os pagamentos
     res.status(200).json(payments);
+  } catch (err) {
+    console.error("Houve um erro: ", err);
+    res.status(500).json({ error: "Ocorreu um erro no servidor" });
+  }
+}
+
+//funcao para cadastrar um novo metodo de pagamento
+export async function createPaymentMethod(req, res) {
+  try {
+    let { name } = req.body;
+
+    //converte o nome para minusculo
+    name = name.toLowerCase();
+
+    //verifica se o metodo de pagamento ja existe
+    const existingPaymentMethod = await findByName(name);
+    if (existingPaymentMethod) {
+      return res
+        .status(400)
+        .json({ error: "Método de pagamento já cadastrado" });
+    }
+
+    //cadastra o metodo de pagamento no banco
+    const result = await createPayment(name);
+
+    res
+      .status(201)
+      .json({
+        message: "Método de pagamento cadastrado com sucesso",
+        id: result.id,
+      });
   } catch (err) {
     console.error("Houve um erro: ", err);
     res.status(500).json({ error: "Ocorreu um erro no servidor" });
