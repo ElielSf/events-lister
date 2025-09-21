d07653f8663b1aa02a6321d0f0c9ac159039f71e
Eliel Souza <elielsf13@gmail.com>
2025-09-21 17:28:53 -0300

    feat: enable event and category routes on server

--- api/src/server.js
+++ api/src/server.js
@@ -7,6 +7,8 @@ import { PORT } from "./config/config.js";
 //importando as rotas do servidor
 import { authRouter } from "./routes/authRoute.js";
 import { paymentRouter } from "./routes/paymentRoute.js";
+import { categoryRouter } from "./routes/categoryRoute.js";
+import { eventRouter } from "./routes/eventRoute.js";
 
 //criando o servidor
 const app = express();
@@ -29,6 +31,12 @@ app.use("/auth", authRouter);
 
 //rota dos metodos de pagamento
 app.use("/payment", authenticateToken, paymentRouter);
+
+//rota dos eventos
+app.use("/payment", authenticateToken, eventRouter);
+
+//rota das categorias
+app.use("/payment", authenticateToken, categoryRouter);
 
 //iniciando o servidor
 const server = app.listen(PORT, () => {
