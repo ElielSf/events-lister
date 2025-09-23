a48f6c38bf3401b15dbc3fcdf8f43dad77871c8b
Eliel Souza <elielsf13@gmail.com>
2025-09-23 18:35:49 -0300

    feat: add header

---
+++ web/src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+//importacoes nativas
+import { useState, useEffect } from "react";
+
+//lib
+import { Link, useNavigate } from "react-router-dom";
+
+//css
+import "./css/Header.css";
+
+//componente com o cabecalho da pagina
+export default function Header() {
+  const [role, setRole] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+
+    if (!token) {
+      notify("error", "Você precisa estar logado.");
+      navigate("/login");
+      return;
+    }
+
+    const role = localStorage.getItem("role");
+
+    setRole(role);
+  });
+
+  return (
+    <header className="header">
+      <button className="header__btn" onClick={() => navigate(-1)}>
+        Voltar
+      </button>
+
+      <nav className="header__nav">
+        <Link to={role === "admin" ? "/admin/menu" : "/menu"}>Menu</Link>
+        {role === "admin" && <Link className="header__nav__item" to="/admin/payments">Pagamentos</Link>}
+        {role === "admin" && <Link to="/admin/categories">Categorias</Link>}
+        {role === "admin" && <Link to="/admin/events">Eventos</Link>}
+      </nav>
+    </header>
+  );
+}
