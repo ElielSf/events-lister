dcc6a60edf58d577918e91bd5559b0d28a2b875b
Eliel Souza <elielsf13@gmail.com>
2025-09-23 15:25:49 -0300

    feat: add admin events page + css

---
+++ web/src/pages/AdminEventsPage/AdminEventsPage.jsx
@@ -0,0 +1,14 @@
+//importando componentes
+import AdminEvents from "../../components/AdminEvents/AdminEvents.jsx";
+
+//css
+import "./css/AdminEventsPage.css";
+
+//exportando pagina de controle dos eventos
+export default function AdminEventsPage() {
+  return (
+    <div className="admin-events-page">
+      <AdminEvents />
+    </div>
+  );
+}
